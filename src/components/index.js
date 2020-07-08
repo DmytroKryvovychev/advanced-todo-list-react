3ab65fcc1cce8549d7e5ae9f8b6baa72fa7e8700
Dmitro Krivovichev <angeldemon90@mail.ru>
2020-07-08 18:17:56 +0300

    New download

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as AddButtonList } from './AddListButton';
+export { default as Badge } from './Badge';
+export { default as List } from './List';
+export { default as Tasks } from './Tasks';
